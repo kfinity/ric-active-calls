30b0c997f5fc779eb508261c84296a98635aa744
Kevin Finity <5213606+kfinity@users.noreply.github.com>
2023-02-10 17:25:53 -0500

    add ssl workaround

--- ac_scraper.py
+++ ac_scraper.py
@@ -13,10 +13,31 @@ from bs4 import BeautifulSoup
 import math
 import time
 import os
+import ssl
+import urllib3
 
 filename = "data/ric_active_calls.csv"
 ac_url = f"https://apps.richmondgov.com/applications/activecalls/Home/ActiveCalls?_={int(time.time())}"
 
+# https://stackoverflow.com/questions/71603314/ssl-error-unsafe-legacy-renegotiation-disabled
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
+
+def get_legacy_session():
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+    session = requests.session()
+    session.mount('https://', CustomHttpAdapter(ctx))
+    return session
 
 # A quick/dirty function to convert HTML jQuery DataTables to dataframes
 def jq_datatables(raw_html, table_id): 
@@ -35,7 +56,8 @@ def jq_datatables(raw_html, table_id):
 
     return df
 
-r = requests.get(ac_url)
+#r = requests.get(ac_url)
+r = get_legacy_session().get(ac_url)
 try:
     ac_df = jq_datatables(r.text, 'tblActiveCallsListing')
 except Exception as e:
